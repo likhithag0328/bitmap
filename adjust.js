57e4518cc70f96e209105429841a90d52be76bd6
likhi_g <183710719+likhithag0328@users.noreply.github.com>
2025-03-16 02:58:33 +0530

    added change brightness feature

--- adjust.js
+++ adjust.js
@@ -15,7 +15,7 @@ class Adjust {
     this.#pixelData = [];
   }
 
-  async processImg() {
+  async processImg(type, value) {
     this.#image = (await Deno.open(this.#inputFile)).readable;
     this.#output = await Deno.open(this.#outputFile, {
       create: true,
@@ -25,39 +25,53 @@ class Adjust {
     const transform = new TransformStream({
       transform: (chunk, controller) => {
         if (this.#isFirst) {
-          this.readHeaderAndPixelData(chunk);
+          this.#readHeaderAndPixelData(chunk);
         } else {
           this.#pixelData.push(...chunk);
         }
 
-        const pixels = this.processPixels();
+        const pixels = this.#processPixels(type, value);
         controller.enqueue(new Uint8Array([...this.#meta, ...pixels]));
-        this.clear();
+        this.#clear();
       },
     });
 
     this.#image.pipeThrough(transform).pipeTo(this.#output.writable);
   }
 
-  readHeaderAndPixelData(chunk) {
+  #readHeaderAndPixelData(chunk) {
     this.#meta.push(...chunk.slice(0, 54));
     this.#pixelData.push(...chunk.slice(54));
     this.#isFirst = false;
   }
 
-  clear() {
+  #clear() {
     this.#meta = [];
     this.#pixelData = [];
   }
 
-  processPixels() {
+  #adjust = {
+    brightness(pixel, value) {
+      const adjustedPixel = pixel + value;
+      return Math.max(0, Math.min(255, adjustedPixel));
+    },
+  };
+
+  #processPixels(type, value) {
+    if (!this.#adjust[type]) {
+      throw new Error(`Unknown adjustment type: ${type}`);
+    }
+
     const pixels = [];
     this.#pixelData.forEach((pixel) => {
-      const adjustedPixel = pixel === 0 ? 0 : Math.abs(pixel - 100);
+      const adjustedPixel = pixel === 0 ? 0 : this.#adjust[type](pixel, value);
       pixels.push(adjustedPixel);
     });
     return pixels;
   }
 }
+
 const processor = new Adjust(Deno.args[0], Deno.args[1]);
-await processor.processImg();
+const type = Deno.args[2] || "brightness";
+const value = parseInt(Deno.args[3]) || 0;
+await processor.processImg(type, value);
