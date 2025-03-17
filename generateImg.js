33ede9f4f0bf1495e9780165dd7f81b7a42b2b23
likhi_g <183710719+likhithag0328@users.noreply.github.com>
2025-03-18 01:01:12 +0530

    generated header

---
+++ generateImg.js
@@ -0,0 +1,111 @@
+class ImageGenerator {
+  constructor(outputFile, height, width) {
+    this.outputFile = outputFile;
+    this.meta = {};
+    this.meta.height = height;
+    this.meta.width = width;
+  }
+
+  async generateBMP() {
+    const fileSize = this.meta.fileSize;
+    const bmpHeader = new Uint8Array([
+      0x42,
+      0x4D,
+      fileSize & 0xFF,
+      (fileSize >> 8) & 0xFF,
+      (fileSize >> 16) & 0xFF,
+      (fileSize >> 24) & 0xFF,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x36,
+      0x00,
+      0x00,
+      0x00,
+    ]);
+
+    await this.writer.write(bmpHeader);
+  }
+
+  generateMetaInfo() {
+    this.meta.bpp = 3;
+    this.meta.padding = (4 - (this.meta.width * this.meta.bpp) % 4) % 4;
+    this.meta.rowWidth = (this.meta.width * this.meta.bpp) + this.meta.padding;
+    this.meta.pixelDataSize = this.meta.rowWidth * this.meta.height;
+    this.meta.fileSize = 54 + this.meta.pixelDataSize;
+  }
+
+  async generateDIB() {
+    const width = this.meta.width;
+    const height = this.meta.height;
+    const pixelDataSize = this.meta.pixelDataSize;
+
+    const dibHeader = new Uint8Array([
+      0x28,
+      0x00,
+      0x00,
+      0x00,
+      width & 0xFF,
+      (width >> 8) & 0xFF,
+      (width >> 16) & 0xFF,
+      (width >> 24) & 0xFF,
+      height & 0xFF,
+      (height >> 8) & 0xFF,
+      (height >> 16) & 0xFF,
+      (height >> 24) & 0xFF,
+      0x01,
+      0x00,
+      0x18,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      pixelDataSize & 0xFF,
+      (pixelDataSize >> 8) & 0xFF,
+      (pixelDataSize >> 16) & 0xFF,
+      (pixelDataSize >> 24) & 0xFF,
+      0x13,
+      0x0B,
+      0x00,
+      0x00,
+      0x13,
+      0x0B,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+      0x00,
+    ]);
+    await this.writer.write(dibHeader);
+  }
+
+  async generateHeader() {
+    this.generateMetaInfo();
+    await this.generateBMP();
+    await this.generateDIB();
+  }
+
+  async generate() {
+    this.output = await Deno.open(this.outputFile, {
+      create: true,
+      write: true,
+    });
+    this.writer = this.output.writable.getWriter();
+
+    await this.generateHeader();
+  }
+}
+
+const main = async () => {
+  const generator = new ImageGenerator("idk.txt", 2, 2);
+  await generator.generate();
+};
+
+main();
